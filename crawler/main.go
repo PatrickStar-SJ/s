0dcb179a26a8b07c860a1b58c5618e2f3ab642dc
JUN SUN <18810256350@163.com>
2024-02-03 19:22:51 +0800

    second commit

--- crawler/main.go
+++ crawler/main.go
@@ -1,37 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"net/http"
+	"spiders_on_go/crawler/engine"
+	"spiders_on_go/crawler/zhenai/parser"
 )
 
 func main() {
-	resp, err := http.Get(
-		"http://localhost:8080/mock/www.zhenai.com/zhenghun")
-	if err != nil {
-		panic(err)
-	}
 
-	//defer resp.Body.Close() 的作用就是无论函数如何返回，都确保 HTTP 响应体会被关闭
-	//
-	//defer 是 Go 语言提供的一种用于处理成对的操作的机制，如打开和关闭、连接和断开连接、加锁和释放锁。
-	//通过 defer 语句，我们可以在函数执行完毕后（返回之前）执行一段代码，无论函数因为什么原因返回，都会执行这个 defer 语句。
-	//
-	//resp.Body.Close()  用来关闭 HTTP 响应体的，这是一个必要的步骤，否则可能会导致内存泄漏。
-	//使用 http.Get 发送请求时，它会返回一个响应（resp）,在读取完数据后，你需要关闭这个流，以释放系统资源。
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Println("Error: status code", resp.StatusCode)
-		return
-	}
-
-	all, err := io.ReadAll(resp.Body)
-	fmt.Printf("Received body: %s\n", all)
-	if err != nil {
-		panic(err)
-	}
-
+	engine.Run(engine.Request{
+		Url:        "http://localhost:8080/mock/www.zhenai.com/zhenghun",
+		ParserFunc: parser.ParseCityList,
+	})
 }
---
+++ crawler/zhenai/parser/citylist_test.go
@@ -0,0 +1,59 @@
+package parser
+
+import (
+	"os"
+	"testing"
+)
+
+func TestParseCityList(t *testing.T) {
+	/*
+		// 先执行下面这段，将结果复制粘贴落盘成   citylist_test_data.html文件
+		contents, err := fetcher.Fetch(
+			"http://localhost:8080/mock/www.zhenai.com/zhenghun")
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%s\n", contents)
+	*/
+
+	contents, err := os.ReadFile(
+		"citylist_test_data.html")
+	if err != nil {
+		panic(err)
+	}
+	//fmt.Printf("%s\n", contents)
+
+	result := ParseCityList(contents)
+	//fmt.Println(len(result.Requests))  //437
+
+	const resultSize = 437
+	expectedUrls := []string{
+		"http://localhost:8080/mock/www.zhenai.com/zhenghun/binhaixin",
+		"http://localhost:8080/mock/www.zhenai.com/zhenghun/binzhou",
+		"http://localhost:8080/mock/www.zhenai.com/zhenghun/bishan",
+	}
+	expectedCities := []string{
+		"滨海新", "滨州", "璧山",
+	}
+
+	if len(result.Items) != resultSize {
+		t.Errorf("result should have %d "+
+			"requests; but had %d",
+			resultSize, len(result.Items))
+	}
+	for i, url := range expectedUrls {
+		if result.Requests[i].Url != url {
+			t.Errorf("expected url #%d: %s; but"+
+				"was %s",
+				i, url, result.Requests[i].Url)
+		}
+	}
+	for i, city := range expectedCities {
+		if result.Items[i].(string) != city {
+			t.Errorf("expected city #%d: %s; but "+
+				"was %s",
+				i, city, result.Items[i].(string))
+		}
+	}
+
+}
