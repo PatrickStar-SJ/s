95867c61c23a06b70639df88faf2134b4bda59ae
JUN SUN <18810256350@163.com>
2024-02-24 23:36:18 +0800

    update log

--- crawler/engine/concurrent.go
+++ crawler/engine/concurrent.go
@@ -4,29 +4,37 @@ import (
 	"log"
 )
 
+// 定义了ConcurrentEngine结构体，包含了调度器接口和工作协程数量。
 type ConcurrentEngine struct {
 	Scheduler   Scheduler
 	WorkerCount int
 }
+
+// 定义了Scheduler接口，要求实现Submit和ConfigureMasterWorkerChan方法。
 type Scheduler interface {
 	Submit(Request)
 	ConfigureMasterWorkerChan(chan Request)
 }
 
+// Run方法是并发引擎的核心，它首先创建请求和解析结果的通道，然后配置调度器，并为每个工作协程创建一个 worker 。
 func (e *ConcurrentEngine) Run(seeds ...Request) {
 
-	//log.Printf("===0====")
+	// 创建了两个通道，in用于传递请求，out用于接收解析结果。
 	in := make(chan Request)
 	out := make(chan ParseResult)
+
+	// 配置调度器，将in通道设置为调度器和工作协程之间的通信通道。
 	e.Scheduler.ConfigureMasterWorkerChan(in)
+
+	// 根据指定的工作协程数量，创建相应数量的工作协程。
 	for i := 0; i < e.WorkerCount; i++ {
 		createWorker(in, out)
 	}
-	//log.Printf("===1====")
+
+	// 将初始请求提交给调度器。
 	for _, r := range seeds {
 		e.Scheduler.Submit(r)
 	}
-	//log.Printf("===2====")
 	itemCount := 0
 	for {
 		result := <-out
@@ -40,9 +48,11 @@ func (e *ConcurrentEngine) Run(seeds ...Request) {
 	}
 }
 
+// 函数创建一个工作协程，不断从in通道接收请求，调用worker函数处理请求，并将结果发送到out通道
 func createWorker(
 	in chan Request, out chan ParseResult) {
 	go func() {
+		// 无限循环，从out通道接收解析结果，打印项目，并将新的请求提交给调度器。
 		for {
 			request := <-in
 			result, err := worker(request)
--- crawler/scheduler/simple.go
+++ crawler/scheduler/simple.go
@@ -2,16 +2,19 @@ package scheduler
 
 import "spiders_on_go/crawler/engine"
 
+// 定义了SimpleScheduler结构体，包含一个工作协程通道。
 type SimpleScheduler struct {
 	workerChan chan engine.Request
 }
 
+// Submit方法用于提交请求，它通过一个goroutine将请求发送到工作协程通道，避免直接发送导致的阻塞。
 func (s *SimpleScheduler) Submit(
 	r engine.Request) {
 	//s.workerChan <- r   //会卡死，视频 16-3开头,16-2结尾。没看懂.换成下面开gorutine
 	go func() { s.workerChan <- r }() // 关键是这一句：每个request会建一个gorutine，这个gorutine的作用就是往共用的channel去分发
 }
 
+// 用于配置主工作协程通道，实际上就是将调度器内部的工作协程通道设置为外部传入的通道。
 func (s *SimpleScheduler) ConfigureMasterWorkerChan(
 	c chan engine.Request) {
 	s.workerChan = c
